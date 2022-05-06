0269934c25bbeed9b061d55dab9850e0842445be
Evanosaure <93705384+Evanosaure@users.noreply.github.com>
2022-05-06 12:44:35 +0200

    Add files via upload

---
+++ Algo_CARTES_TREHIN.py
@@ -0,0 +1,40 @@
+class Carte :
+    def __init__ (self, mana, cardname, description)
+        self.mana = mana
+        self.cardname = cardname
+        self.description = description
+
+class Mage :
+    def __init__ (self, manavalue, name, pv, main, defausse, playground)
+        self.manavalue = manavalue
+        self.name = name
+        self.pv = pv
+        self.main = main
+        self.defausse = defausse
+        self.playground = playground
+
+class Cristal :
+    def __init__ (self, mana, cardname, description, value)
+        self.mana = mana
+        self.cardname = cardname
+        self.description = description
+        self.value =value
+
+class Creature :
+    def __init__ (self, mana, cardname, description, pv, damage)
+        self.mana = mana
+        self.cardname = cardname
+        self.description = description
+        self.pv = pv
+        self.damge = damage
+
+class Blast :
+    def __init__ (self, mana, cardname, description, value)
+        self.mana = mana
+        self.cardname = cardname
+        self.description = description
+        self.value =value
+
+
+if blast = 1 
+    pv - value
